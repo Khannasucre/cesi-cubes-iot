066aae23ea763b8cc7e96b539adbdbedc06ab3b7
Lou Gitta <lougitta@gmail.com>
2023-12-29 15:59:32 +0100

    update of flask api

--- flask/api.py
+++ flask/api.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,  render_template
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 
-class SensorData(db.Model):
+class Meteo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temperature = db.Column(db.Float)
     humidity = db.Column(db.Float)
@@ -16,21 +17,37 @@ class SensorData(db.Model):
     
 @app.route('/', methods=['GET'])
 def home():
-    return "Hello, this is the home page!"
+    return "<h1>Coucou, si tu arrives içi tu es au bon endroit jeune DI</h1><h3>Nous te reverrons Juju!</h3><h6>Et toi loic c'est pas grave si on ne te vois plus</h6>"
 
-@app.route('/api/sensor_data', methods=['GET'])
-def get_sensor_data():
-    data = SensorData.query.all()
-    sensor_data = [{'temperature': item.temperature, 'humidity': item.humidity, 'pressure': item.pressure} for item in data]
-    return jsonify(sensor_data)
+@app.route('/api/meteo/', methods=['GET', 'POST'])
 
-@app.route('/api/store_data', methods=['POST'])
-def store_data():
-    data = request.get_json()
-    new_data = SensorData(temperature=data['temperature'], humidity=data['humidity'], pressure=data['pressure'])
-    db.session.add(new_data)
-    db.session.commit()
-    return jsonify(message='Data stockées mon rhoya')
+def meteo():
+
+    if request.method == 'GET':
+
+        data = Meteo.query.order_by(Meteo.id.desc()).all()
+        meteo = [{'temperature': item.temperature, 'humidity': item.humidity, 'pressure': item.pressure} for item in data]
+        return render_template('meteo.html', meteo=meteo)
+        #return jsonify(meteo)
+
+    elif request.method == 'POST':
+        
+        data = request.get_json()
+        new_data = Meteo(temperature=data['temperature'], humidity=data['humidity'], pressure=data['pressure'])
+        db.session.add(new_data)
+        db.session.commit()
+        return jsonify(message='Data dans le baba mon Khoya')
+
+    else:
+        return jsonify(message='Pas la bonne method mon crampté au sucres :( !')
+
+# @app.route('/api/store_data/', methods=['POST'])
+# def store_data():
+#     data = request.get_json()
+#     new_data = Meteo(temperature=data['temperature'], humidity=data['humidity'], pressure=data['pressure'])
+#     db.session.add(new_data)
+#     db.session.commit()
+#     return jsonify(message='Data stockées mon Khoya')
 
 if __name__ == '__main__':
     with app.app_context():
