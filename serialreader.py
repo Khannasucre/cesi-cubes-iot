5c455344e5c08df61dbe4a33f425aafe5b4d1def
LouGitta <lougitta@gmail.com>
2024-01-24 13:03:29 +0100

    ajout du lecteur de l'Arduino et ajout d'une donnée temporelle

--- serialreader.py
+++ serialreader.py
@@ -1,8 +1,13 @@
-
+from datetime import datetime
 import requests
 import serial
+import time
 
-url = 'http://10.244.128.66:8000/api/v1/meteo/'
+url = 'http://127.0.0.1:8000/api/v1/meteo/'
+
+timestamp = int(time.time()) + 3600
+date =  datetime.utcfromtimestamp(timestamp)
+date = date.strftime('%Y-%m-%d %H:%M:%S')
 
 port = 'COM3' 
 baud_rate = 115200
@@ -11,16 +16,21 @@ ser = serial.Serial(port, baud_rate)
 
 while True:
     line = ser.readline().decode('utf-8').strip()
-
-    a = line.split("/")
-    data = {
-        "temperature" : a[0],
-        "humidity" : a[1],
-        "pressure" : a[2]
-    }
-
-    if data.values():
-        post = requests.post(url,json=data)
-        print(post.content)
-    else:
-        print('no data')
+    print(line)
+    if line[:5] == "Found":
+        print("Not now")
+    else :
+        a = line.split("/")
+        print(a)
+        data = {
+            "temperature" : float(a[0]),
+            "humidity" : float(a[1]),
+            "pressure" : float(a[2]),
+            "timestamp" : date
+        }
+        print(data)
+        if data.values():
+            post = requests.post(url,json=data)
+            print(post.content)
+        else:
+            print('no data')
